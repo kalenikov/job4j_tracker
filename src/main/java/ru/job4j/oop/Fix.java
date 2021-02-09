6a0bda02e4605a4667e31ec766d68c9b1e253cf7
serj <sergeykalenikov@gmail.com>
2021-02-09 16:21:53 +0300

    1.7. Конструктор [#462055]

---
+++ src/main/java/ru/job4j/oop/Fix.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop;
+
+public class Fix {
+    private String desc;
+
+    public Fix(String desc) {
+        this.desc = desc;
+    }
+
+    public static void main(String[] args) {
+        Fix fix1 = new Fix("new fix");
+    }
+}
