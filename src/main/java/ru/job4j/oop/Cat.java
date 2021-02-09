12d2a56f93a557f4a413b3023996938f0d4b56a3
serj <sergeykalenikov@gmail.com>
2021-02-09 11:39:55 +0300

    1.1. Создание объекта [#462048]

---
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,8 @@
+package ru.job4j.oop;
+
+public class Cat {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat();
+        Cat cat2 = new Cat();
+    }
+}
---
+++ src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class Dog {
+    public static void main(String[] args) {
+        Dog dog1 = new Dog();
+        Dog dog2 = new Dog();
+        Dog dog3 = new Dog();
+    }
+}
