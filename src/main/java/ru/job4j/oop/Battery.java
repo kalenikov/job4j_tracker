e65ffe79a850bc49588a6a2c243225ebb202e590
serj <sergeykalenikov@gmail.com>
2021-02-10 12:37:40 +0300

    1.8. Состояние объекта [#462054]

--- src/main/java/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -8,8 +8,8 @@ public class Battery {
     }
 
     public void exchange(Battery another) {
-        this.load = this.load + another.load;
-        another.load = 0;
+        another.load = this.load + another.load;
+        this.load = 0;
     }
 
     public static void main(String[] args) {
