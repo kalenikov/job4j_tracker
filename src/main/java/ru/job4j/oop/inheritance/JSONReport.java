3a0ac91f32ed516e13ce2b48ce16f43c16ca04ec
serj <sergeykalenikov@gmail.com>
2021-02-16 16:11:11 +0300

    4. Переопределение [#462061]

---
+++ src/main/java/ru/job4j/oop/inheritance/JSONReport.java
@@ -0,0 +1,11 @@
+package ru.job4j.oop.inheritance;
+
+public class JSONReport extends TextReport {
+
+    public String generate(String name, String body) {
+        return "{" + System.lineSeparator() +
+                "name: " + name + ", " + System.lineSeparator() +
+                "body: " + body + System.lineSeparator() +
+                "}";
+    }
+}
