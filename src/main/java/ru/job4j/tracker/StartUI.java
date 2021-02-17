d30397594b2573672e2b4a8cd47569caa4f24642
serj <sergeykalenikov@gmail.com>
2021-02-17 16:47:46 +0300

    5. Tracker - хранилище [#462077]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -7,5 +7,10 @@ public class StartUI {
         Item item = new Item();
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         System.out.println(dtf.format(item.getCreated()));
+
+        Tracker tracker = new Tracker();
+        tracker.add(item);
+
+        System.out.println(tracker.findById(1));
     }
 }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,8 @@
 package ru.job4j.tracker;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -12,14 +15,27 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        Item rsl = null;
         for (int index = 0; index < size; index++) {
             Item item = items[index];
             if (item.getId() == id) {
-                rsl = item;
-                break;
+                return item;
             }
         }
-        return rsl;
+        return null;
     }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+    public Item findByName(String name) {
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (name.equals(item.getName())) {
+                return item;
+            }
+        }
+        return null;
+    }
+
 }
