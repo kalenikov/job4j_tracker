7a2d049642781d55e44432133757f61ed3c90f15
serj <sergeykalenikov@gmail.com>
2021-02-22 19:22:55 +0300

    0. Что такое исключение. [#462025]

---
+++ src/main/java/ru/job4j/ex/FrequentEx.java
@@ -0,0 +1,10 @@
+package ru.job4j.ex;
+
+public class FrequentEx {
+    public static void main(String[] args) {
+        String[] shops = {"Ebay", "Amazon", "Ozon"};
+        for (int index = 0; index <= shops.length; index++) {
+            System.out.println(shops[index]);
+        }
+    }
+}
