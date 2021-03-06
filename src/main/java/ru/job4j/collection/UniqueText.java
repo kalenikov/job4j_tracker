01c988d731add08bcc41727d57ac19b0cc0693b5
serj <sergeykalenikov@gmail.com>
2021-03-06 09:32:57 +0300

    2. Идентичные тексты[212655#461990]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,20 @@
+package ru.job4j.collection;
+
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String s : origin) {
+            check.add(s);
+        }
+        for (String s :text) {
+            if (!check.contains(s)) return false;
+        }
+        return rsl;
+    }
+}
