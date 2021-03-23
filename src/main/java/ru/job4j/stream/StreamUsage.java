a3aa028be599f8dcac383ff467cd615ef7180205
serj <sergeykalenikov@gmail.com>
2021-03-23 10:34:14 +0300

    0. Stream API[58166#461808]

---
+++ src/main/java/ru/job4j/stream/StreamUsage.java
@@ -0,0 +1,16 @@
+package ru.job4j.stream;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamUsage {
+    public static void main(String[] args) {
+        List<Integer> rsl = new ArrayList<>(Arrays.asList(10, -20, 30, -40, 50));
+        Stream<Integer> stream = rsl.stream();
+        rsl = stream.filter(x -> x > 0).collect(Collectors.toList());
+        System.out.println(rsl);
+    }
+}
