5e1b90eebc877c5418e36d4e166c85667e3fa0f7
serj <sergeykalenikov@gmail.com>
2021-04-03 18:44:55 +0300

    1. Экзамен. Коллекции. [#461968]

--- src/main/java/ru/job4j/collection/FreezeStr.java
+++ src/main/java/ru/job4j/collection/FreezeStr.java
@@ -4,17 +4,21 @@ import java.util.*;
 
 public class FreezeStr {
     public static boolean eq(String left, String right) {
-        //разбиваем строку в массив символов,
-        //собираем в мапу(Символ, Количество в строке) и сравниваем
-        return stringToMap(left).equals(stringToMap(right));
-    }
-
-    public static Map<Character, Integer> stringToMap(String string) {
-        var rsl = new HashMap<Character, Integer>();
-        for (Character c : string.toCharArray()) {
-            rsl.putIfAbsent(c, 0);
-            rsl.compute(c, (k, v) -> ++v);
+        var leftMap = new HashMap<Character, Integer>();
+        for (Character c : left.toCharArray()) {
+            leftMap.putIfAbsent(c, 0);
+            leftMap.compute(c, (k, v) -> ++v);
         }
-        return rsl;
+        for (Character c : right.toCharArray()) {
+            if (!leftMap.containsKey(c)) {
+                return false;
+            }
+            if (leftMap.get(c) == 1) {
+                leftMap.remove(c);
+            } else {
+                leftMap.compute(c, (k, v) -> --v);
+            }
+        }
+        return leftMap.isEmpty();
     }
 }
